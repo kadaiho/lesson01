ca96cf40fe36eefe91927db3c1aed3c8fe1604f2
kadaiho <d.murakami.gude@gmail.com>
2020-07-28 17:07:33 +0900

    図形作成を修正しました。

--- 3-2/main.js
+++ 3-2/main.js
@@ -1,20 +1,20 @@
 const arg1 = Number(process.argv[2]);
 const arg2 = Number(process.argv[3]);
-const yoko = "*-";
-const tate = "-*";
+const even = "*-";
+const odd = "-*";
 
 for (let i = 0; i < arg1; i++) {
   if (i % 2 === 0) {
     if (arg2 % 2 === 0) {
-      console.log(`${yoko.repeat(arg2 / 2)}`);
+      console.log(`${even.repeat(arg2 / 2)}`);
     } else {
-      console.log(`${yoko.repeat(arg2 / 2)}*`);
+      console.log(`${even.repeat(arg2 / 2)}*`);
     }
   } else {
     if (arg2 % 2 === 0) {
-      console.log(`${tate.repeat(arg2 / 2)}`);
+      console.log(`${odd.repeat(arg2 / 2)}`);
     } else {
-      console.log(`${tate.repeat(arg2 / 2)}-`);
+      console.log(`${odd.repeat(arg2 / 2)}-`);
     }
   }
 }
