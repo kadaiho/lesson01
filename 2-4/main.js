739a1252fd95e4db26c1b86e4b12ff6f20c2cc17
kadaiho <d.murakami.gude@gmail.com>
2020-07-28 11:56:32 +0900

    季節を表示するを修正しました。

--- 2-4/main.js
+++ 2-4/main.js
@@ -2,39 +2,23 @@ const arg1 = process.argv[2];
 const str = String(arg1);
 switch (str) {
   case "1":
+  case "11":
+  case "12":
     console.log("冬");
     break;
   case "2":
-    console.log("春");
-    break;
   case "3":
-    console.log("春");
-    break;
   case "4":
     console.log("春");
     break;
   case "5":
-    console.log("夏");
-    break;
   case "6":
-    console.log("夏");
-    break;
   case "7":
     console.log("夏");
     break;
   case "8":
-    console.log("秋");
-    break;
   case "9":
-    console.log("秋");
-    break;
   case "10":
     console.log("秋");
-    break;
-  case "11":
-    console.log("冬");
-    break;
-  case "12":
-    console.log("冬");
     break;
 }
