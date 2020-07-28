0005e73ccd251e93e6f853a3b1194e77df7bccc1
kadaiho <d.murakami.gude@gmail.com>
2020-07-28 12:21:54 +0900

    お支払いを修正しました。

--- 2-5/main.js
+++ 2-5/main.js
@@ -4,3 +4,4 @@ const juu = Math.floor((arg1 % 100) / 10);
 const ichi = Math.floor((arg1 % 100) % 10);
 console.log(`100円玉${hyaku}枚, 10円玉${juu}枚, 1円玉${ichi}枚`);
 //console.log("百円玉" + hyaku + "枚");
+//syue
