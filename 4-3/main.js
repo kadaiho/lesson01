b24eaaa0f35683c1536a43fe37516fee6969b01d
kadaiho <d.murakami.gude@gmail.com>
2020-08-07 13:26:38 +0900

    ログファイルの中からエラーの内容だけを抜き出して表示するを実装しました。

--- 4-3/main.js
+++ 4-3/main.js
@@ -1,5 +1,16 @@
 const errorLogs = [
   "Error 101:Invalid Exception AAA - 2020/01/01/01:00",
   "Error 3:Invalid Exception BBBB - 2020/01/01/02:00",
-  "Error 22:Invalid Exception CCCCC - 2020/01/01/03:00"
-]
+  "Error 22:Invalid Exception CCCCC - 2020/01/01/03:00",
+];
+//最初に見つかるコロン":"からスペース＋ハイフン" -"の位置を見つける
+for (let i = 0; i < errorLogs.length; i++) {
+  const str = String(errorLogs[i]);
+  //  console.log(str.indexOf(":"));
+  //  console.log(str.indexOf(" -"));
+  //見つけた位置から文字列を切り出す
+  const position = str.indexOf(":");
+  const position2 = str.indexOf(" -");
+  console.log(str.substring(position + 1, position2));
+}
+//const str = String(errorLogs);
