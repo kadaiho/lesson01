e95d2257b69d2db783b4ceb7679c7a7ec35fc9bc
kadaiho <d.murakami.gude@gmail.com>
2020-08-11 16:38:28 +0900

    ランダムな数を

--- 4-4/main.js
+++ 4-4/main.js
@@ -1,10 +1,11 @@
 var random = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 const number = [];
-for (let i = 0; i < 100; i++) {
+while (number.length < 10) {
   var num = random[Math.floor(Math.random() * random.length)];
 
   if (!number.includes(num)) {
     number.push(num);
   }
 }
+
 console.log(number.join(` `));
