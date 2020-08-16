97016f571fd41e141f0b6df3eefccd2da9422319
kadaiho <d.murakami.gude@gmail.com>
2020-08-16 13:20:59 +0900

    小さい順に並び替えを修正しました。

--- 4-5/main.js
+++ 4-5/main.js
@@ -1,20 +1,19 @@
-const arg1 = Number();
 const array = [];
-const num = [];
+const sortedArray = [];
 for (let i = 2; i < process.argv.length; i++) {
-  const arg1 = process.argv[i];
+  const arg1 = Number(process.argv[i]);
   //console.log(arg1);
   array.push(arg1);
 }
-var num2 = Number(Math.min(...array));
+var fewestArray = Math.min(...array);
 //console.log(num2);
 //console.log(array);
-for (let a = num2; a < 9999; a++) {
+for (let a = fewestArray; a < 9999; a++) {
   for (let j = 0; j < array.length; j++) {
-    if (a === Number(array[j])) {
-      num.push(a);
+    if (a === array[j]) {
+      sortedArray.push(a);
     }
   }
 }
-console.log(num);
+console.log(sortedArray);
 //node main.js 2 13 42 5 25 59 1 4 13 12 11 10 2
