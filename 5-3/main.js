10f36f9a6ab05ac6c9b99e16fc07ec977a9f3d88
kadaiho <d.murakami.gude@gmail.com>
2020-08-17 15:32:01 +0900

    ２つのデータを合わせるを実装しました。

---
+++ 5-3/main.js
@@ -0,0 +1,17 @@
+const jsonPlayData = require(`./playdata.json`);
+const jsonSongs = require(`./songs.json`);
+const data = jsonPlayData.data;
+const songs = jsonSongs.songs;
+const array = [];
+
+data.sort(function (a, b) {
+  return b.count - a.count;
+});
+for (let i = 0; i < data.length; i++) {
+  const found = songs.find((song) => {
+    return Number(song.id) === Number(data[i].id);
+  });
+  console.log(`${i + 1}位:${found.title}\t${data[i].count}`);
+}
+
+//**playdataのcountが大きい順にソートして、曲のtitleとplaydataのcountを[順位:${title}\t${count}]のフォーマットで表示してください**
